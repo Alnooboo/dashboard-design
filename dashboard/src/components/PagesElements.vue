18e001ec4d122f80ebab96e7ee48323b04a56a82
Alnooboo <101204241+Alnooboo@users.noreply.github.com>
2024-07-08 15:07:54 +0300

    Update PagesElements.vue

--- dashboard/src/components/PagesElements.vue
+++ dashboard/src/components/PagesElements.vue
@@ -1,205 +1,213 @@
 <script setup></script>
 
 <template>
-  <li class="flex py-2 px-3">
-    <svg
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M12.833 2.75V6.41667C12.833 6.92293 13.2434 7.33333 13.7497 7.33333H17.4163"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M15.583 19.25H6.41634C5.40382 19.25 4.58301 18.4292 4.58301 17.4167V4.58333C4.58301 3.57081 5.40382 2.75 6.41634 2.75H12.833L17.4163 7.33333V17.4167C17.4163 18.4292 16.5955 19.25 15.583 19.25Z"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-    <p class="px-2">Pages</p>
-    <svg
-      class="ml-auto"
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8.25 5.5L13.75 11L8.25 16.5"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
+  <li>
+    <button class="w-236 flex items-center py-2 px-3">
+      <svg
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12.833 2.75V6.41667C12.833 6.92293 13.2434 7.33333 13.7497 7.33333H17.4163"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M15.583 19.25H6.41634C5.40382 19.25 4.58301 18.4292 4.58301 17.4167V4.58333C4.58301 3.57081 5.40382 2.75 6.41634 2.75H12.833L17.4163 7.33333V17.4167C17.4163 18.4292 16.5955 19.25 15.583 19.25Z"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+      <p class="px-2">Pages</p>
+      <svg
+        class="ml-auto"
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.25 5.5L13.75 11L8.25 16.5"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
   </li>
-  <li class="flex items-center py-2 px-3">
-    <svg
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M11 2.75C13.1411 4.64436 15.9357 5.63069 18.7916 5.5C19.6317 8.35774 19.2769 11.434 17.8084 14.0255C16.3399 16.617 13.8832 18.5021 11 19.25C8.11671 18.5021 5.66001 16.617 4.1915 14.0255C2.72299 11.434 2.36821 8.35774 3.20829 5.5C6.06419 5.63069 8.85878 4.64436 11 2.75"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <ellipse
-        cx="10.9997"
-        cy="10.0834"
-        rx="0.916667"
-        ry="0.916667"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <path
-        d="M11.0003 11V13.2917"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-    <p class="px-2">Authentications</p>
-    <svg
-      class="ml-auto"
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8.25 5.5L13.75 11L8.25 16.5"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
+  <li>
+    <button class="w-236 flex items-center py-2 px-3">
+      <svg
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M11 2.75C13.1411 4.64436 15.9357 5.63069 18.7916 5.5C19.6317 8.35774 19.2769 11.434 17.8084 14.0255C16.3399 16.617 13.8832 18.5021 11 19.25C8.11671 18.5021 5.66001 16.617 4.1915 14.0255C2.72299 11.434 2.36821 8.35774 3.20829 5.5C6.06419 5.63069 8.85878 4.64436 11 2.75"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <ellipse
+          cx="10.9997"
+          cy="10.0834"
+          rx="0.916667"
+          ry="0.916667"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M11.0003 11V13.2917"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+      <p class="px-2">Authentications</p>
+      <svg
+        class="ml-auto"
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.25 5.5L13.75 11L8.25 16.5"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
   </li>
-  <li class="flex items-center py-2 px-3">
-    <svg
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <ellipse
-        cx="4.58366"
-        cy="11"
-        rx="0.916667"
-        ry="0.916667"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <circle
-        cx="10.9997"
-        cy="11"
-        r="0.916667"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <circle
-        cx="17.4167"
-        cy="11"
-        r="0.916667"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-    <p class="px-2">Wizard Examples</p>
-    <svg
-      class="ml-auto"
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8.25 5.5L13.75 11L8.25 16.5"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
+  <li>
+    <button class="w-236 flex items-center py-2 px-3">
+      <svg
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <ellipse
+          cx="4.58366"
+          cy="11"
+          rx="0.916667"
+          ry="0.916667"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <circle
+          cx="10.9997"
+          cy="11"
+          r="0.916667"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <circle
+          cx="17.4167"
+          cy="11"
+          r="0.916667"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+      <p class="px-2">Wizard Examples</p>
+      <svg
+        class="ml-auto"
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.25 5.5L13.75 11L8.25 16.5"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
   </li>
-  <li class="flex items-center py-2 px-3">
-    <svg
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect
-        x="3.66699"
-        y="3.66669"
-        width="14.6667"
-        height="14.6667"
-        rx="2"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-    <p class="px-2">Dialog Examples</p>
-    <svg
-      class="ml-auto"
-      width="22"
-      height="22"
-      viewBox="0 0 22 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8.25 5.5L13.75 11L8.25 16.5"
-        stroke="#2F2B3D"
-        stroke-opacity="0.9"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
+  <li>
+    <button class="w-236 flex items-center py-2 px-3">
+      <svg
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="3.66699"
+          y="3.66669"
+          width="14.6667"
+          height="14.6667"
+          rx="2"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+      <p class="px-2">Dialog Examples</p>
+      <svg
+        class="ml-auto"
+        width="22"
+        height="22"
+        viewBox="0 0 22 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.25 5.5L13.75 11L8.25 16.5"
+          stroke="#2F2B3D"
+          stroke-opacity="0.9"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
   </li>
 </template>
 
